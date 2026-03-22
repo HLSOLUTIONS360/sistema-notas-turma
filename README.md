0b18ac498a61e54b3bb93ee2d493928a92f5c223
HLSOLUTIONS360º <hlsolutions360.br@gmail.com>
2026-03-22 14:18:21 -0300

    Ajuste Henrique

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sistema-notas-turma
 Projeto da turma: Sistema de Notas em JavaScript Professor: Heitor Santos Pereira
+
---
+++ dados.js
@@ -0,0 +1,13 @@
+// variáveis globais do sistema
+
+let quantidade = 0;
+
+let nomes = [];
+
+let notas = [];
+
+let soma = 0;
+
+let media = 0;
+
+let situacoes = [];
