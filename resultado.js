265661df329fa867bdfcaee99081c7aa9b02f48b
CLECIANEM2020 <contatocleciisilva@gmail.com>
2026-03-22 17:51:39 -0300

    resutado.

--- resultado.js
+++ resultado.js
@@ -5,7 +5,7 @@ for (let i = 0; i < nomes.length; i++) {
   saida += `${nomes[i]} - Nota: ${notas[i]} - ${situacoes[i]}<br>`;
 }
 
-resultado.innerHTML = `
+resultado.innerHTML =`
 ${saida}
 <br>
 Média da turma: ${media.toFixed(2)}
